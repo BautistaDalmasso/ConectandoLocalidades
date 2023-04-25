c999ece2472591190d41aac7c5682d32f3d7df63
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-04-25 13:33:47 -0300

    Pequeños arreglos.

--- src/tests/ArbolGeneradorMinimoTest.java
+++ src/tests/ArbolGeneradorMinimoTest.java
@@ -9,7 +9,27 @@ import negocio.Localidad;
 
 public class ArbolGeneradorMinimoTest {
 	@Test
-	public void arbolGeneradorMinimoSeObtieneCorrectamenteTest() {
+	public void arbolGeneradorMinimoSinLocalidadesTest() {
+		GrafoCompletoLocalidades grafoCompleto = new GrafoCompletoLocalidades();
+		
+		grafoCompleto.construirArbolGeneradorMinimo();
+		
+		Assert.assertEquals(0, grafoCompleto.getArbolGeneradorMinimo().getTamanio());
+	}
+	
+	@Test
+	public void arbolGeneradorMinimoUnaLocalidadTest() {
+		Localidad localidadCentro = new Localidad("centro", "Buenos Aires", 0, 0);
+		GrafoCompletoLocalidades grafoCompleto = new GrafoCompletoLocalidades();
+		grafoCompleto.agregarLocalidad(localidadCentro);
+		
+		grafoCompleto.construirArbolGeneradorMinimo();
+		
+		Assert.assertEquals(1, grafoCompleto.getArbolGeneradorMinimo().getTamanio());
+	}
+	
+	@Test
+	public void arbolGeneradorMinimoTresLocalidadesTest() {
 		Localidad localidadCentro = new Localidad("centro", "Buenos Aires", 0, 0);
 		Localidad localidadOeste = new Localidad("Este", "Buenos Aires", -1, 0);
 		Localidad localidadEste = new Localidad("Oeste", "Buenos Aires", 1, 0);
