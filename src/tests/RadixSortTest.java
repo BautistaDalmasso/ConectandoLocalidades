fa9a8c561c1b0c695f2fa654a127c116ae29f471
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-04-29 10:11:18 -0300

    Refactoriza los tests de RadixSort.

--- src/tests/RadixSortTest.java
+++ src/tests/RadixSortTest.java
@@ -7,73 +7,48 @@ import org.junit.Test;
 import radixsort.AristaPesadaParaTests;
 import radixsort.RadixSort;
 
-
 public class RadixSortTest {
 	@Test
 	public void arregloOrdenadoNoCambiaTest() {
-		AristaPesadaParaTests[] arregloInput = {
-				new AristaPesadaParaTests(9),
-				new AristaPesadaParaTests(99),
-				new AristaPesadaParaTests(999),
-				new AristaPesadaParaTests(1000),
-		};
-		AristaPesadaParaTests[] arregloEsperado = {
-				new AristaPesadaParaTests(9),
-				new AristaPesadaParaTests(99),
-				new AristaPesadaParaTests(999),
-				new AristaPesadaParaTests(1000),
-		};
-		
+		AristaPesadaParaTests[] arregloInput = crearArregloDeAristas(new int[] { 9, 99, 999, 1000 });
+
+		AristaPesadaParaTests[] arregloEsperado = crearArregloDeAristas(new int[] { 9, 99, 999, 1000 });
+
 		RadixSort.ordenar(arregloInput, 1000);
-		
+
 		assertArrayEquals(arregloInput, arregloEsperado);
 	}
-	
+
 	@Test
 	public void arregloInvertidoEsOrdenadoTest() {
-		AristaPesadaParaTests[] arregloInput = {
-				new AristaPesadaParaTests(1000),
-				new AristaPesadaParaTests(999),
-				new AristaPesadaParaTests(99),
-				new AristaPesadaParaTests(9),
-		};
-		AristaPesadaParaTests[] arregloEsperado = {
-				new AristaPesadaParaTests(9),
-				new AristaPesadaParaTests(99),
-				new AristaPesadaParaTests(999),
-				new AristaPesadaParaTests(1000),
-		};
-		
+		AristaPesadaParaTests[] arregloInput = crearArregloDeAristas(new int[] { 1000, 999, 99, 9 });
+
+		AristaPesadaParaTests[] arregloEsperado = crearArregloDeAristas(new int[] { 9, 99, 999, 1000 });
+
 		RadixSort.ordenar(arregloInput, 1000);
-		
+
 		assertArrayEquals(arregloInput, arregloEsperado);
 	}
-	
+
 	@Test
 	public void arregloEsOrdenadoTest() {
-		AristaPesadaParaTests[] arregloInput = {
-				new AristaPesadaParaTests(180),
-				new AristaPesadaParaTests(10),
-				new AristaPesadaParaTests(20),
-				new AristaPesadaParaTests(1),
-				new AristaPesadaParaTests(3),
-				new AristaPesadaParaTests(564),
-				new AristaPesadaParaTests(7),
-				new AristaPesadaParaTests(34),
-		};
-		AristaPesadaParaTests[] arregloEsperado = {
-				new AristaPesadaParaTests(1),
-				new AristaPesadaParaTests(3),
-				new AristaPesadaParaTests(7),
-				new AristaPesadaParaTests(10),
-				new AristaPesadaParaTests(20),
-				new AristaPesadaParaTests(34),
-				new AristaPesadaParaTests(180),
-				new AristaPesadaParaTests(564),
-		};
-		
+		AristaPesadaParaTests[] arregloInput = crearArregloDeAristas(new int[] { 180, 10, 20, 1, 3, 564, 7, 109, 34 });
+
+		AristaPesadaParaTests[] arregloEsperado = crearArregloDeAristas(
+				new int[] { 1, 3, 7, 10, 20, 34, 109, 180, 564 });
+
 		RadixSort.ordenar(arregloInput, 564);
-		
+
 		assertArrayEquals(arregloEsperado, arregloInput);
- 	}
+	}
+
+	private AristaPesadaParaTests[] crearArregloDeAristas(int[] pesos) {
+		AristaPesadaParaTests[] ret = new AristaPesadaParaTests[pesos.length];
+
+		for (int i = 0; i < pesos.length; i++) {
+			ret[i] = new AristaPesadaParaTests(pesos[i]);
+		}
+
+		return ret;
+	}
 }
